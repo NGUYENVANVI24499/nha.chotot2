a914cf2951102e4252b3e24554716deba0ff6bb6
Nguyễn Văn Vĩ <43158136+NGUYENVANVI24499@users.noreply.github.com>
2022-04-12 21:49:53 +0700

    update

---
+++ .history/assets/js/slider_20220412161146.js
@@ -0,0 +1,90 @@
+$('.slider-container').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    arrows:false
+  });
+
+  
+  // $('.cards').slick({
+  //   infinite: true,
+  //   slidesToShow: 5,
+  //   slidesToScroll: 1,
+  //   autoplay:true,
+  //   arrows:false
+  // });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  });
+
+
+
+
+  const a =document.querySelector('.info-extend');
+  const b = document.querySelector('.info-container');
+
+  a.onclick = function(){
+    b.classList.toggle('show');
+    if(b.classList.contains('show'))
+    {
+      a.innerText='thu gọn';
+    }else{
+      a.innerText='mở rộng';
+    }
+  }
+
+  const hiden = document.querySelector('.menu-subnav');
+  const abc= document.querySelector('menu-item');
+  abc.onclick = function(){
+    hiden.classList.toggle('acctive');
+  }
+  
